7a6c01eb2d8e46533ef14da05bdb43142733ce44
barathmohan3 <45123296+barathmohan3@users.noreply.github.com>
2025-06-13 15:48:00 +0530

    Update main.tf

--- modules/helm/main.tf
+++ modules/helm/main.tf
@@ -1,4 +1,3 @@
-# Fetch EKS cluster details dynamically
 data "aws_eks_cluster" "cluster" {
   name = var.cluster_id
 }
@@ -7,14 +6,12 @@ data "aws_eks_cluster_auth" "cluster" {
   name = var.cluster_id
 }
 
-# Kubernetes Provider
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.cluster.token
 }
 
-# Helm Provider
 provider "helm" {
   kubernetes {
     host                   = data.aws_eks_cluster.cluster.endpoint
